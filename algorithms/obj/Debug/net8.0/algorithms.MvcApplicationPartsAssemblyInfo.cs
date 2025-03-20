204d4b005c57da2001409d6d85da14bff23fd11a
LesLis94 <solncuivetru@gmail.com>
2025-03-20 21:04:51 +0300

    test

--- algorithms/obj/Debug/net8.0/algorithms.MvcApplicationPartsAssemblyInfo.cs
+++ algorithms/obj/Debug/net8.0/algorithms.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("algorithms")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c4f4595bcbc655a9aa0b4641f483a721f64e96e")]
-[assembly: System.Reflection.AssemblyProductAttribute("algorithms")]
-[assembly: System.Reflection.AssemblyTitleAttribute("algorithms")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Создано классом WriteCodeFragment MSBuild.
 

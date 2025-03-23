7cd427ef06b7c5a137e0c017daaaafa2fbfd2f3c
LesLis94 <solncuivetru@gmail.com>
2025-03-23 23:43:18 +0300

    разбор строки

---
+++ Controllers/Calcul.cs
@@ -0,0 +1,19 @@
+﻿using algorithms.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace algorithms.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculController : Controller
+    {
+        [HttpPost]
+        public async Task<ActionResult> Calculate(string expression)
+        {
+
+            var result = new CalculService(expression);
+
+            return Ok();
+        }
+    }
+}
---
+++ Services/CalculService.cs
@@ -0,0 +1,31 @@
+﻿namespace algorithms.Services
+{
+    public class CalculService
+    {
+        public CalculService(string text) 
+        {
+            string stringDecimal = "";
+
+            foreach (var item in text)
+            {
+                if (item == ' ') continue;
+
+                if (item == '(') continue;
+
+                if(char.IsDigit(item) || item == '.')
+                {
+                    stringDecimal += item;
+                }
+                if (!string.IsNullOrEmpty(stringDecimal))
+                {
+                    decimal.TryParse(stringDecimal, out decimal number);
+                }
+            }
+        }
+
+        private decimal RecursionLambda()
+        {
+            return 0;
+        }
+    }
+}
